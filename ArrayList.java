343684405cdbace2247589d4be6f85cd3c58857d
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-13 22:33:38 +0000

    Added isEmpty(), size() and get()

---
+++ ArrayList.java
@@ -0,0 +1,32 @@
+public ArrayList implements List {
+	private Object[] objectArray;
+
+	public ArrayList() {
+		this.objectArray = new Object[10];
+	}
+
+	public boolean isEmpty() {
+		if(objectArray == null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public int size() {
+		return objectArray.length();
+	}
+
+	public ReturnObject get(int index) {
+		new ReturnObjectImpl array = new ReturnObjectImpl(objectArray);
+		if(objectArray.length() == 0) {
+			array.setError(ErrorMessage.EMPTY_STRUCTURE);
+			return array;
+		}else if(index >= objectArray.length() || index < 0) {
+			array.setError(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+			return array;
+		}else {
+			return array;
+		}
+	}
+}
