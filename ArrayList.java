978389fec46d1c96929b1fe90d21175d4a2473db
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-14 20:29:29 +0000

    Refactored get(). Added in remove() and first add()

--- ArrayList.java
+++ ArrayList.java
@@ -1,28 +1,48 @@
 public class ArrayList implements List {
 	private Object[] objectArray = new Object[10];
+	private int size = 0;
 
 	public boolean isEmpty() {
-		if(objectArray == null) {
-			return true;
-		}else {
-			return false;
-		}
+		return (size() == 0);
 	}
 
 	public int size() {
-		return objectArray.length();
+		return size;
 	}
 
 	public ReturnObject get(int index) {
-		ReturnObjectImpl array = new ReturnObjectImpl(objectArray);
-		if(objectArray.length() == 0) {
-			array.setError(ErrorMessage.EMPTY_STRUCTURE);
-			return array;
-		}else if(index >= objectArray.length() || index < 0) {
-			array.setError(ErrorMessage.INDEX_OUT_OF_BOUNDS);
-			return array;
+		ReturnObjectImpl array = new ReturnObjectImpl(objectArray[index]);
+		array.setErrorList(isEmpty(), size, index);
+		if (array.getError() != ErrorMessage.NO_ERROR) {
+			return array.gerError();
 		}else {
-			return array;
+			return array.getReturnValue();
 		}
 	}
-}
+
+	public ReturnObject remove(int index) {
+		ReturnObjectImpl array = new ReturnObjectImpl(objectArray[index]);
+		array.setErrorList (isEmpty(), size, index);
+		if (array.getError() != ErrorMessage.NO_ERROR) {
+			return array.getError();
+		}else{
+			for(int i = index, i < size(), i++) {
+				objectArray[i] = objectArray[i+1];
+			}i--;
+			return array.getReturnValue();
+		}
+	}
+
+	public ReturnObject add(int index, Object item) {
+		ReturnObjectImpl array = new ReturnObjectImpl(item);
+		array.setErrorList (isEmpty(), size, index, item);
+		if (array.getError() != ErrorMessage.NO_ERROR) {
+			return array.getError();
+		}else {
+			for (int i = index, i < size(), i++) {
+				objectArray[i+1] = objectArray[i];
+			}objectArray[index] = item;
+			i++;
+			return null;
+		}
+	}
