2238183c9d1f57be5d25b20e1d58df0d333ca89d
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-15 20:01:17 +0000

    Added head() and rest()

---
+++ FunctionalArrayList.java
@@ -0,0 +1,15 @@
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+	public ReturnObject head() {
+		ReturnObjectImpl result = new ReturnObjectImpl(get(0).getReturnValue());
+		result.setErrorList(isEmpty(), size(), 0);
+		return result;
+	}
+
+	public FunctionalList rest() {
+		FunctionalArrayList result = new FunctionalArrayList();
+		for (int i = 1; i < size(); i++) {
+			result.add(get(i).getReturnValue());
+		}
+		return result;
+	}
+}
