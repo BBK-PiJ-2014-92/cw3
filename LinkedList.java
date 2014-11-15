ab9d6ca0b29a462793331c250e2a5d51035e8d06
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-15 03:00:09 +0000

    Added remove()

--- LinkedList.java
+++ LinkedList.java
@@ -27,11 +27,33 @@ public class LinkedList implements List {
 		if (linked.getError() != ErrorMessage.NO_ERROR) {
 			return linked;
 		}else {
-			for (int i = 0, i < index, i++) {
+			for (int i = 0; i < index; i++) {
 				tempNode = tempNode.getNextNode();
+				tempNode.setNextNode(null);
 				linked = new ReturnObjectImpl(tempNode);
 			}
 			return linked;
 		}
 	}
 
+	public ReturnObject remove(int index) {
+		Node tempNode = listHead;
+		ReturnObjectImpl linked = new ReturnObjectImpl(tempNode);
+		linked.setErrorList(isEmpty(), size(), index);
+		if (linked.getError() != ErrorMessage.NO_ERROR) {
+			return linked;
+		}else {
+			if (index = 0) {
+				listHead = listHead.getNextNode();
+				return linked = new ReturnObjectImpl(listHead);
+			}else {
+				for (int i = 0; i < index - 1; i++) {
+					tempNode = tempNode.getNextNode();
+				}
+				Node removedNode = tempNode.getNextNode();
+				tempNode.setNextNode(removedNode.getNextNode());
+				removedNode.setNextNode(null);
+				return linked = new ReturnObjectImpl(removedNode);
+			}
+		}
+	}
