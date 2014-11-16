55140f16bc62792d9aae3b97093eb625cba36486
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-16 13:57:03 +0000

    Created SampleableList implementation with sample() method

---
+++ SampleableListImpl.java
@@ -0,0 +1,13 @@
+public class SampleableListImpl extends AbstractList implements SampleableList {
+	public SampleableListImpl(List listType) {
+		super(listType);
+	}
+
+	public SampleableList sample() {
+		SampleableList sample = new SampleableListImpl (getListType());
+		for (int i = 0; i < size(); i += 2) {
+			sample.add(get(i));
+		}
+		return sample;
+	}
+}
