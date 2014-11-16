5f8f8d199c1881b9a7bd1984f01aab8183d39ee3
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-16 13:46:34 +0000

    Created a new abstract class for lists

---
+++ AbstractList.java
@@ -0,0 +1,31 @@
+public abstract class AbstractList implements List{
+	private List listType;
+
+	public AbstractList(List listType) {
+		this.listType = listType;
+	}
+
+	public boolean isEmpty() {
+		return listType.isEmpty();
+	}
+
+	public int size() {
+		return listType.size();
+	}
+
+	public ReturnObject remove(int index) {
+		return listType.remove(index);
+	}
+
+	public ReturnObject add(int index, Object item) {
+		return listType.add(index, item);
+	}
+
+	public ReturnObject add(Object item) {
+		return listType.add(item);
+	}
+
+	public List getListType() {
+		return listType;
+	}
+}
