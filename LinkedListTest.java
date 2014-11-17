68f800a7b4e84a40f0706db7b7e0f6548e0135f0
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-17 00:07:07 +0000

    Created new testing file with all the same tests as ArrayListTest, but for LinkedLists

---
+++ LinkedListTest.java
@@ -0,0 +1,156 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class LinkedListTest{
+	private List linked;
+
+	public LinkedListTest() {
+		linked = new ArrayList();
+	}
+	
+	
+	@After
+	public void cleanUp() {
+		linked = null;
+	}
+
+	@Test
+	public void addTest1() {
+		linked.add(1);
+		Object actual = linked.get(0).getReturnValue();
+		Object expected = 1;
+		assertEquals(actual, expected); //works
+		
+		linked.add(null);
+		actual = linked.get(1).getReturnValue();
+		expected = null;
+		assertEquals(actual, expected); //works
+		
+		linked.add(2);
+		actual = linked.get(1).getReturnValue();
+		expected = 2;
+		assertEquals(actual, expected); //works
+		
+		linked.add("test");
+		actual = linked.get(2).getReturnValue();
+		expected = "test";
+		assertEquals(actual, expected); //works
+
+	}
+	
+	@Test
+	public void addTest2() {
+		linked.add(0, 1);
+		Object actual = linked.get(0).getReturnValue();
+		assertNull(actual); //works
+		
+		linked.add(0);
+		linked.add(0, "head");
+		actual = linked.get(0).getReturnValue();
+		Object expected = "head";
+		assertEquals(actual, expected); //works
+		
+		actual = linked.get(1).getReturnValue();
+		expected = 0;
+		assertEquals(actual, expected); //works
+		
+		linked.add(1, null);
+		actual = linked.get(1).getReturnValue();
+		assertNotNull(actual); //works
+		
+		linked.add(5, "OutOfBounds");
+		actual = linked.get(5).getReturnValue();
+		assertNull(actual); //works
+		
+		linked.add(1, "second");
+		actual = linked.get(2).getReturnValue();
+		expected = 0;
+		assertEquals(actual, expected); //works
+		
+		linked.add(4); //stored in 3
+		linked.add(5); //stored in 4
+		linked.add(6); //stored in 5
+		linked.add(7); //stored in 6
+		linked.add(8); //stored in 7
+		linked.add(9); //stored in 8
+		linked.add(10); //store in 9
+		
+		linked.add("Is this full yet?");
+		actual = linked.get(10).getReturnValue();
+		expected = "Is this full yet?";
+		assertEquals(actual, expected); //works
+		
+		linked.add(10, "How about now?");
+		actual = linked.get(11).getReturnValue();
+		assertEquals(actual, expected); //works
+		
+		actual = linked.add("give me null or give me death").getReturnValue();
+		assertNull(actual); //works
+		
+		actual = linked.add(10, "another null test").getReturnValue();
+		assertNull(actual); //works
+	}
+	@Test
+	public void emptyTest() {
+		boolean actual = linked.isEmpty();
+		assertTrue(actual); //works
+		
+		linked.add("Not empty");
+		actual = linked.isEmpty();
+		assertFalse(actual); //works
+		
+		linked.remove(0);
+		actual = linked.isEmpty();
+		assertTrue(actual); //works
+		
+		//actual = linkedTest.isEmpty();
+		//assertTrue(actual);
+	}
+
+	@Test
+	public void sizeTest() {
+		int actual = linked.size();
+		int expected = 0;
+		assertEquals(actual, expected); //works
+		
+		linked.add("size 1");
+		actual = linked.size();
+		expected = 1;
+		assertEquals(actual, expected); //works
+		
+		linked.remove(0);
+		actual = linked.size();
+		expected = 0;
+		assertEquals(actual, expected); //works
+		
+		linked.remove(0);
+		actual = linked.size();
+		assertEquals(actual, expected); //works
+		
+		//actual = linkedTest.size();
+		//assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void removeTest() {
+		linked.add(0);
+		linked.add(1);
+		linked.add(2);
+		linked.add(3);
+		linked.remove(3);
+		Object actual = linked.get(3).getReturnValue();
+		assertNull(actual); //works
+		
+		linked.remove(0);
+		actual = linked.get(0).getReturnValue();
+		Object expected = 1;
+		assertEquals(actual, expected); //works
+		
+		actual = linked.remove(2).getReturnValue();
+		assertNull(actual); //works
+		
+		
+		
+	}
+
+}
--- ListTest.java
+++ ListTest.java
@@ -2,116 +2,105 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ListTest{
-	private List arrayTest;
-	//private List linkedTest;
+	private List testBoth;
 
 	public ListTest() {
-		arrayTest = new ArrayList();
-		//linkedTest = new LinkedList(); 
+		testBoth = new ArrayList();
 	}
 	
-	//@Before
-	//public void buildUp() {
-		
-		//linkedTest.add("1");
-		//linkedTest.add("2");
-		//linkedTest.add("3");
-		//linkedTest.add("4");
-		//linkedTest.add("5");
-	//}
 	
 	@After
 	public void cleanUp() {
-		arrayTest = null;
-		//linkedTest = null;
+		testBoth = null;
 	}
 
 	@Test
 	public void addTest1() {
-		arrayTest.add(1);
-		Object actual = arrayTest.get(0).getReturnValue();
+		testBoth.add(1);
+		Object actual = testBoth.get(0).getReturnValue();
 		Object expected = 1;
 		assertEquals(actual, expected); //works
 		
-		arrayTest.add(null);
-		actual = arrayTest.get(1).getReturnValue();
+		testBoth.add(null);
+		actual = testBoth.get(1).getReturnValue();
 		expected = null;
 		assertEquals(actual, expected); //works
 		
-		arrayTest.add(2);
-		actual = arrayTest.get(1).getReturnValue();
+		testBoth.add(2);
+		actual = testBoth.get(1).getReturnValue();
 		expected = 2;
 		assertEquals(actual, expected); //works
 		
-		arrayTest.add("test");
-		actual = arrayTest.get(2).getReturnValue();
+		testBoth.add("test");
+		actual = testBoth.get(2).getReturnValue();
 		expected = "test";
 		assertEquals(actual, expected); //works
+
 	}
 	
 	@Test
 	public void addTest2() {
-		arrayTest.add(0, 1);
-		Object actual = arrayTest.get(0).getReturnValue();
+		testBoth.add(0, 1);
+		Object actual = testBoth.get(0).getReturnValue();
 		assertNull(actual); //works
 		
-		arrayTest.add(0);
-		arrayTest.add(0, "head");
-		actual = arrayTest.get(0).getReturnValue();
+		testBoth.add(0);
+		testBoth.add(0, "head");
+		actual = testBoth.get(0).getReturnValue();
 		Object expected = "head";
 		assertEquals(actual, expected); //works
 		
-		actual = arrayTest.get(1).getReturnValue();
+		actual = testBoth.get(1).getReturnValue();
 		expected = 0;
 		assertEquals(actual, expected); //works
 		
-		arrayTest.add(1, null);
-		actual = arrayTest.get(1).getReturnValue();
+		testBoth.add(1, null);
+		actual = testBoth.get(1).getReturnValue();
 		assertNotNull(actual); //works
 		
-		arrayTest.add(5, "OutOfBounds");
-		actual = arrayTest.get(5).getReturnValue();
+		testBoth.add(5, "OutOfBounds");
+		actual = testBoth.get(5).getReturnValue();
 		assertNull(actual); //works
 		
-		arrayTest.add(1, "second");
-		actual = arrayTest.get(2).getReturnValue();
+		testBoth.add(1, "second");
+		actual = testBoth.get(2).getReturnValue();
 		expected = 0;
 		assertEquals(actual, expected); //works
 		
-		arrayTest.add(4); //stored in 3
-		arrayTest.add(5); //stored in 4
-		arrayTest.add(6); //stored in 5
-		arrayTest.add(7); //stored in 6
-		arrayTest.add(8); //stored in 7
-		arrayTest.add(9); //stored in 8
-		arrayTest.add(10); //store in 9
+		testBoth.add(4); //stored in 3
+		testBoth.add(5); //stored in 4
+		testBoth.add(6); //stored in 5
+		testBoth.add(7); //stored in 6
+		testBoth.add(8); //stored in 7
+		testBoth.add(9); //stored in 8
+		testBoth.add(10); //store in 9
 		
-		arrayTest.add("Is this full yet?");
-		actual = arrayTest.get(10).getReturnValue();
+		testBoth.add("Is this full yet?");
+		actual = testBoth.get(10).getReturnValue();
 		expected = "Is this full yet?";
 		assertEquals(actual, expected); //works
 		
-		arrayTest.add(10, "How about now?");
-		actual = arrayTest.get(11).getReturnValue();
+		testBoth.add(10, "How about now?");
+		actual = testBoth.get(11).getReturnValue();
 		assertEquals(actual, expected); //works
 		
-		actual = arrayTest.add("give me null or give me death").getReturnValue();
+		actual = testBoth.add("give me null or give me death").getReturnValue();
 		assertNull(actual); //works
 		
-		actual = arrayTest.add(10, "another null test").getReturnValue();
+		actual = testBoth.add(10, "another null test").getReturnValue();
 		assertNull(actual); //works
 	}
 	@Test
 	public void emptyTest() {
-		boolean actual = arrayTest.isEmpty();
+		boolean actual = testBoth.isEmpty();
 		assertTrue(actual); //works
 		
-		arrayTest.add("Not empty");
-		actual = arrayTest.isEmpty();
+		testBoth.add("Not empty");
+		actual = testBoth.isEmpty();
 		assertFalse(actual); //works
 		
-		arrayTest.remove(0);
-		actual = arrayTest.isEmpty();
+		testBoth.remove(0);
+		actual = testBoth.isEmpty();
 		assertTrue(actual); //works
 		
 		//actual = linkedTest.isEmpty();
@@ -120,22 +109,22 @@ public class ListTest{
 
 	@Test
 	public void sizeTest() {
-		int actual = arrayTest.size();
+		int actual = testBoth.size();
 		int expected = 0;
 		assertEquals(actual, expected); //works
 		
-		arrayTest.add("size 1");
-		actual = arrayTest.size();
+		testBoth.add("size 1");
+		actual = testBoth.size();
 		expected = 1;
 		assertEquals(actual, expected); //works
 		
-		arrayTest.remove(0);
-		actual = arrayTest.size();
+		testBoth.remove(0);
+		actual = testBoth.size();
 		expected = 0;
 		assertEquals(actual, expected); //works
 		
-		arrayTest.remove(0);
-		actual = arrayTest.size();
+		testBoth.remove(0);
+		actual = testBoth.size();
 		assertEquals(actual, expected); //works
 		
 		//actual = linkedTest.size();
@@ -144,20 +133,20 @@ public class ListTest{
 	
 	@Test
 	public void removeTest() {
-		arrayTest.add(0);
-		arrayTest.add(1);
-		arrayTest.add(2);
-		arrayTest.add(3);
-		arrayTest.remove(3);
-		Object actual = arrayTest.get(3).getReturnValue();
+		testBoth.add(0);
+		testBoth.add(1);
+		testBoth.add(2);
+		testBoth.add(3);
+		testBoth.remove(3);
+		Object actual = testBoth.get(3).getReturnValue();
 		assertNull(actual); //works
 		
-		arrayTest.remove(0);
-		actual = arrayTest.get(0).getReturnValue();
+		testBoth.remove(0);
+		actual = testBoth.get(0).getReturnValue();
 		Object expected = 1;
 		assertEquals(actual, expected); //works
 		
-		actual = arrayTest.remove(2).getReturnValue();
+		actual = testBoth.remove(2).getReturnValue();
 		assertNull(actual); //works
 		
 		
