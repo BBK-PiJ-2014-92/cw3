690f0506843cfe53f55e625f964f8bfa0f9e26b6
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-17 00:23:47 +0000

    Created a Stack implementation

---
+++ StackImpl.java
@@ -0,0 +1,28 @@
+public class StackImpl extends AbstractStack implements Stack {
+	private int size;
+
+	public StackImpl(List list) {
+		super(list);
+	}
+
+	public boolean isEmpty() {
+		return (size() == 0);
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public void push(Object item) {
+		internalList.add(item);
+		size++;
+	}
+
+	public ReturnObject top() {
+		return internalList.get(size - 1);
+	}
+
+	public ReturnObject pop() {
+		return internalList.remove(size - 1);
+	}
+}
