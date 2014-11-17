7663d453b46250b6b05bb874ebfa43a75a70286d
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-11-17 01:50:36 +0000

    All addTest2() asserts work

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -32,62 +32,62 @@ public class LinkedListTest{
 		linked.add("test");
 		actual = linked.get(2).getReturnValue();
 		expected = "test";
-		assertEquals(actual, expected);
+		assertEquals(actual, expected); //works
 
 	}
 	
-	//@Test
-	//public void addTest2() {
-		//linked.add(0, 1);
-		//Object actual = linked.get(0).getReturnValue();
-		//assertNull(actual); 
+	@Test
+	public void addTest2() {
+		linked.add(0, 1);
+		Object actual = linked.get(0).getReturnValue();
+		assertNull(actual); 
 		
-		//linked.add(0);
-		//linked.add(0, "head");
-		//actual = linked.get(0).getReturnValue();
-		//Object expected = "head";
-		//assertEquals(actual, expected); 
+		linked.add(0);
+		linked.add(0, "head");
+		actual = linked.get(0).getReturnValue();
+		Object expected = "head";
+		assertEquals(actual, expected); 
 		
-		//actual = linked.get(1).getReturnValue();
-		//expected = 0;
-		//assertEquals(actual, expected); 
+		actual = linked.get(1).getReturnValue();
+		expected = 0;
+		assertEquals(actual, expected); 
 		
-		//linked.add(1, null);
-		//actual = linked.get(1).getReturnValue();
-		//assertNotNull(actual); 
+		linked.add(1, null);
+		actual = linked.get(1).getReturnValue();
+		assertNotNull(actual); 
 		
-		//linked.add(5, "OutOfBounds");
-		//actual = linked.get(5).getReturnValue();
-		//assertNull(actual); 
+		linked.add(5, "OutOfBounds");
+		actual = linked.get(5).getReturnValue();
+		assertNull(actual); 
 		
-		//linked.add(1, "second");
-		//actual = linked.get(2).getReturnValue();
-		//expected = 0;
-		//assertEquals(actual, expected); 
+		linked.add(1, "second");
+		actual = linked.get(2).getReturnValue();
+		expected = 0;
+		assertEquals(actual, expected); 
 		
-		//linked.add(4); //stored in 3
-		//linked.add(5); //stored in 4
-		//linked.add(6); //stored in 5
-		//linked.add(7); //stored in 6
-		//linked.add(8); //stored in 7
-		//linked.add(9); //stored in 8
-		//linked.add(10); //store in 9
+		linked.add(4); //stored in 3
+		linked.add(5); //stored in 4
+		linked.add(6); //stored in 5
+		linked.add(7); //stored in 6
+		linked.add(8); //stored in 7
+		linked.add(9); //stored in 8
+		linked.add(10); //store in 9
 		
-		//linked.add("Is this full yet?");
-		//actual = linked.get(10).getReturnValue();
-		//expected = "Is this full yet?";
-		//assertEquals(actual, expected); 
+		linked.add("Is this full yet?");
+		actual = linked.get(10).getReturnValue();
+		expected = "Is this full yet?";
+		assertEquals(actual, expected); 
 		
-		//linked.add(10, "How about now?");
-		//actual = linked.get(11).getReturnValue();
-		//assertEquals(actual, expected); 
+		linked.add(10, "How about now?");
+		actual = linked.get(11).getReturnValue();
+		assertEquals(actual, expected); 
 		
-		//actual = linked.add("give me null or give me death").getReturnValue();
-		//assertNull(actual); 
+		actual = linked.add("give me null or give me death").getReturnValue();
+		assertNull(actual); 
 		
-		//actual = linked.add(10, "another null test").getReturnValue();
-		//assertNull(actual); 
-	//}
+		actual = linked.add(10, "another null test").getReturnValue();
+		assertNull(actual); 
+	}
 	//@Test
 	//public void emptyTest() {
 		//boolean actual = linked.isEmpty();
